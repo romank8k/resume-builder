0eef72508e80e145327e32719fbc832dce9db17f
Roman Khmelichek <roman.khmelichek@gmail.com>
2025-05-02 12:25:42 -0400

    Update url

--- scrape/scrape.js
+++ scrape/scrape.js
@@ -2,7 +2,7 @@ import scrape from 'website-scraper';
 import SaveToExistingDirectoryPlugin from 'website-scraper-existing-directory';
 
 const result = await scrape({
-  urls: ['http://localhost:8080/resume-html'],
+  urls: ['http://localhost:8080/web/html'],
   directory: '../../resume.romankh.me/',
   plugins: [ new SaveToExistingDirectoryPlugin() ]
 });
