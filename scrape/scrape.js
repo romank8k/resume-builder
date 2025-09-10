246f7c28490833303ded77fd74dff40e9a907c91
Roman Khmelichek <roman.khmelichek@gmail.com>
2025-09-09 21:13:42 -0400

    Explicitly scrape the pdf

--- scrape/scrape.js
+++ scrape/scrape.js
@@ -2,7 +2,7 @@ import scrape from 'website-scraper';
 import SaveToExistingDirectoryPlugin from 'website-scraper-existing-directory';
 
 const result = await scrape({
-  urls: ['http://localhost:8080/web/html'],
+  urls: ['http://localhost:8080/web/html', 'http://localhost:8080/web/resume.pdf'],
   directory: '../../resume.romankh.me/',
   plugins: [ new SaveToExistingDirectoryPlugin() ]
 });
