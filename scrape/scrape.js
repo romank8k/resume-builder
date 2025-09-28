d1edac3d4bf5480d00e280d67d8710197155c99d
Roman Khmelichek <roman.khmelichek@gmail.com>
2025-09-28 14:16:16 -0400

    Use filename option for the pdf, otherwise it's named as an html file

--- scrape/scrape.js
+++ scrape/scrape.js
@@ -2,9 +2,9 @@ import scrape from 'website-scraper';
 import SaveToExistingDirectoryPlugin from 'website-scraper-existing-directory';
 
 const result = await scrape({
-  urls: ['http://localhost:8080/web/html', 'http://localhost:8080/web/resume.pdf'],
-  directory: '../../resume.romankh.me/',
-  plugins: [ new SaveToExistingDirectoryPlugin() ]
+    urls: ['http://localhost:8080/web/html', {url: 'http://localhost:8080/web/resume.pdf', filename: 'resume.pdf'}],
+    directory: '../../resume.romankh.me/',
+    plugins: [new SaveToExistingDirectoryPlugin()]
 });
 
 console.log(result);
